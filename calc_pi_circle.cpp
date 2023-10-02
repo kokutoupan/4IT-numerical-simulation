aabb5959ad126ab08abba5b66f85e684151b5a03
kokutoupan <kokutou2004@gmail.com>
2023-10-02 22:09:45 +0900

    ソースコードの追加

---
+++ calc_pi_circle.cpp
@@ -0,0 +1,45 @@
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numbers>
+#include <string>
+
+constexpr double D_PI = std::numbers::pi;
+
+int main() {
+
+  int DivNum = 1;
+  for (auto loopCount = 1; loopCount < 10; ++loopCount) {
+    DivNum *= 10;
+    double delta = 1.0 / DivNum;
+    std::cout << DivNum << ", ";
+
+    const auto startTime = std::chrono::system_clock::now();
+    double sum = 0;
+    for (int i = 0; i < DivNum; ++i) {
+      const double x = (delta * i);
+      const double area = delta * std::sqrt(1 - x * x);
+      sum += area;
+    }
+    const double pi = sum * 4.0;
+    const auto endTime = std::chrono::system_clock::now();
+    // std::cout << std::setprecision(15) << "PI ~= " << pi << std::endl;
+    // std::cout << "Error [%] = " << (100.0 * std::fabs(D_PI - pi) / D_PI)
+    //           << std::endl;
+    // std::cout << "Elapsed time [ms] = "
+    //           <<
+    //           std::chrono::duration_cast<std::chrono::milliseconds>(endTime
+    //           -
+    //                                                                    startTime)
+    //                  .count()
+    //           << std::endl;
+    std::cout << std::setprecision(15) << pi << ", ";
+    std::cout << (100.0 * std::fabs(D_PI - pi) / D_PI) << ", ";
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime -
+                                                                      startTime)
+                     .count()
+              << std::endl;
+  }
+  return 0;
+}
